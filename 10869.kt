ee3e75eb8e3ea2eae0ebd3dc15b6352f96fa064e
see-ho <siki7878@gmail.com>
2022-11-23 22:42:29 +0900

    sol 10869

---
+++ 10869.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)){
+    var a = nextInt()
+    var b = nextInt()
+    println(a+b)
+    println(a-b)
+    println(a*b)
+    println(a/b)
+    println(a%b)
+}
