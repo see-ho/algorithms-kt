4cad3b61c3c9b22fc90a7a357d3c6274465903ee
see-ho <siki7878@gmail.com>
2022-12-08 21:22:30 +0900

    sol 11022

---
+++ 11022.kt
@@ -0,0 +1,7 @@
+fun main(){
+    val num = readLine()!!.toInt()
+    for(i in 1..num){
+        var arr = readLine()!!.split(" ").map{it.toInt()}
+        println("Case #"+i+": "+arr[0]+" + "+arr[1]+" = "+(arr[0]+arr[1]));
+    }
+}
