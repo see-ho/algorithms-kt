4f26a477b3d86bace633365a1fe0fc09633e5e3f
see-ho <107612802+see-ho@users.noreply.github.com>
2023-03-03 22:45:15 +0900

    sol 2338

---
+++ 2338.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val num1 = readln().toBigInteger()
+    val num2 = readln().toBigInteger()
+    println(num1.plus(num2))
+    println(num1.minus(num2))
+    println(num1.multiply(num2))
+}
