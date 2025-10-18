7d4f1ef040c2a2396da992f86def90fd74253370
이소희 <siki7878@gmail.com>
2025-10-18 21:20:20 +0900

    sol 피보나치 수.kt

---
+++ programmers/피보나치 수.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(n: Int): Int {
+        val arr = IntArray(n+1)
+        arr[0] = 0
+        arr[1] = 1
+        for(i in 2..n){
+            arr[i] = (arr[i-1] + arr[i-2]) % 1234567
+        }
+        return arr[n] 
+    }
+}
