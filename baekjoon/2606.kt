00d822f2a56c28a80a8568b536ca82f96029e817
이소희 <siki7878@gmail.com>
2025-10-24 23:31:40 +0900

    update 2606.kt
    전역 변수 -> 함수 리턴으로 방식 변경

--- baekjoon/2606.kt
+++ baekjoon/2606.kt
@@ -1,3 +1,9 @@
+/**
+ * 2023
+ * - 전역 변수 cnt 사용
+ * - BufferedReader 사용으로 빠른 입출력
+ */
+
 var cnt = 0
 fun main(){
     val br = System.`in`.bufferedReader()
@@ -22,4 +28,33 @@ fun dfs(v:Int, graph:Array<ArrayList<Int>>,visited:BooleanArray){
             dfs(i,graph,visited)
         }
     }
+}
+
+/**
+ * 2025.10.24 
+ * - 전역 변수 제거 → 함수 반환값 구조로 개선
+ * - readln 사용
+ */
+
+ fun main(){
+    val n = readln().toInt()
+    val graph = Array(n+1){ArrayList<Int>()}
+    val visited = BooleanArray(n+1){false}
+
+    repeat(readln().toInt()){
+        val (a,b) = readln().split(" ").map { it.toInt() }
+        graph[a].add(b)
+        graph[b].add(a)
+    }
+
+    fun dfs(now: Int, visited: BooleanArray): Int{
+        visited[now] = true
+        var count = 1
+ 
+        for(i in graph[now]){
+            if(!visited[i]) count += dfs(i,visited)
+        }
+        return count
+    }    
+    print(dfs(1 ,visited) - 1)
 }
