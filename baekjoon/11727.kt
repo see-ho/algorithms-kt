33fb59175e00f376d6a3c617af527358b8fb3691
이소희 <siki7878@gmail.com>
2025-11-14 23:18:23 +0900

    sol 2*n 타일링 2 11727.kt

---
+++ baekjoon/11727.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val n = readln().toInt()
+    val dp = IntArray(n + 2)
+
+    dp[1] = 1
+    dp[2] = 3
+    for (i in 3..n) {
+        dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007
+    }
+
+    println(dp[n])
+}
